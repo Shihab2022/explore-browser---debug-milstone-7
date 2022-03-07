0c67b882e77a40f506e95e42020e1f3340d2182a
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-03-07 20:31:39 +0600

    complete dev tools and debug

--- conceputal-section/dev-tools-and-debug/script.js
+++ conceputal-section/dev-tools-and-debug/script.js
@@ -9,12 +9,10 @@ const handleSearch = () => {
     const url = `https://theaudiodb.com/api/v1/json/2/search.php?s=${keyword.value}`;
     fetch(url)
         .then((res) => res.json())
-        // .then((data) => console.log(data.artists));
         .then((data) => showArtists(data))
 };
 
 const showArtists = (data) => {
-  console.log(data)
     const artistContainer = elementById("artists");
   
     data?.artists?.forEach((artist) =>{
@@ -40,40 +38,32 @@ const showArtists = (data) => {
 
 
 
+const fetchAlbums = (id) => {
+    const url = ` https://theaudiodb.com/api/v1/json/${2}/album.php?i=${id}`;
+    fetch(url)
+        .then((res) => res.json())       
+        .then((data) => showAlbum(data));
+    const artistContainer = elementById("artists");
+    artistContainer.innerHTML = "";
+};
 
 
+const showAlbum = (data) => {
+    const albumContainer = elementById("albums");
+    data.album.forEach((item) => {
+        const div = document.createElement("div");
+        div.classList.add("album");
+        div.innerHTML = `
+        <div class="album-image-container">
+          <img src="${item.strAlbumThumb}"alt=""/>
+        </div>
+        <div class="album-name">
+          <h3>${item.strAlbum}</h3>
+          <h5>${item.intYearReleased}</h5>
+          <h5>${item.strReleaseFormat}</h5>
+        </div>
+      `;
 
-// const fetchAlbums = (id) => {
-//     const url = `theaudiodb.com/api/v1/json/2/album.php?i=${id}`;
-//     fetch(url)
-//         .then((res) => res.JSON())
-//         .then((data) => showAlbum(data));
-//     const artistContainer = elementById("artists");
-//     artistContainer.innerHTML = "";
-// };
-
-
-
-
-
-
-// const showAlbum = (data) => {
-//     const albumContainer = elementById("albums");
-//     album.forEach((item) => {
-//         const div = document.createElement("div");
-//         div.classList.add("album");
-//         div.innerHTML = `
-//         <div class="album-image-container">
-//           <img
-//             src="${album.strAlbumThumb}"
-//             alt=""
-//           />
-//         </div>
-//         <div class="album-name">
-//           <h3>${album.strAlbum}</h3>
-//         </div>
-//       `;
-
-//         albumContainer.appendChild(div);
-//     });
-// };
+        albumContainer.appendChild(div);
+    });
+};
