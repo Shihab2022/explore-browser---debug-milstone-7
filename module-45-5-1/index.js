751b90cb555e0e38e9e7757135575cda1fd7a01a
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-03-09 17:07:08 +0600

    module 45-5-1 question

---
+++ module-45-5-1/index.js
@@ -0,0 +1,16 @@
+const mybutton1 =() =>{
+
+    console.log('buuton 1 is clicked')
+}
+const mybutton2 =() =>{
+
+    console.log('buuton 2 is clicked')
+}
+const mybutton3 =() =>{
+
+    console.log('buuton 3 is clicked')
+}
+const mybutton4 =() =>{
+
+    console.log('buuton 4 is clicked')
+}
